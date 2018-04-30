0dc598fb69ad71cc3b79128ac4887f47871ae791
Teri Forey <teri.evans@nottingham.ac.uk>
2018-04-30 11:33:43 +0100

    Added standard run process

--- testbird/processes/__init__.py
+++ testbird/processes/__init__.py
@@ -2,10 +2,12 @@ from .wps_wordcounter import WordCounter
 from .wps_inout import InOut
 from .wps_testplot import MakeMap
 from .wps_nameparams import RunNAME
+from .wps_name_standard import RunNAMEstandard
 
 processes = [
     WordCounter(),
     InOut(),
     MakeMap(),
     RunNAME(),
+    RunNAMEstandard(),
 ]
--- testbird/processes/wps_name_standard.py
+++ testbird/processes/wps_name_standard.py
@@ -14,7 +14,7 @@ import logging
 LOGGER = logging.getLogger("PYWPS")
 
 
-class RunNAME(Process):
+class RunNAMEstandard(Process):
     """
     Notes
     -----
@@ -24,35 +24,15 @@ class RunNAME(Process):
     """
     def __init__(self):
         inputs = [
-            LiteralInput('latitude', 'Latitude', data_type='float',
-                         abstract="Location of release",
-                         default=-24.867222),
-            LiteralInput('longitude','Longitude', data_type='float',
-                         abstract="Location of release",
-                         default=16.863611),
-            LiteralInput('domain', 'Domain coordinates', data_type='string',
-                         abstract='Coordinates to seach within (minX,maxX,minY,maxY)',
-                         default='-120.0,80.0,-30.0,90.0', min_occurs=0),
-            LiteralInput('elevation','Elevation', data_type='integer',
-                         abstract = "release elevation, m agl for land, m asl for marine release",
-                         default=10, min_occurs=0),
-            LiteralInput('elevation_range_min','Elevation Range Min', data_type='integer',
-                         abstract="Minimum range of elevation",
-                         default=None, min_occurs=0),
-            LiteralInput('elevation_range_max', 'Elevation Range Max', data_type='integer',
-                         abstract = "Maximum range of elevation",
-                         default=None, min_occurs=0),
-            LiteralInput('title', 'Title of run', data_type='string',
-                         abstract = "Title of run"),
+            LiteralInput('title', 'Release Station', data_type='string',
+                         abstract="Weather station of release",
+                         allowed_values=['CAPEVERDE','BEIJING']),
             LiteralInput('runBackwards', 'Run Backwards', data_type='boolean',
                          abstract = 'Whether to run backwards in time (default) or forwards',
                          default = '1', min_occurs=0),
             LiteralInput('time', 'Time to run model over', data_type='integer',
-                         abstract = 'Time',
-                         default=1),
-            LiteralInput('timeFmt','Time format', data_type='string',
-                         abstract='choose whether to measure time in hours or days',
-                         allowed_values = ['days','hours'], default='days'),
+                         abstract = 'Number of days model will run over',
+                         allowed_values=[1,5,10]),
             LiteralInput('elevationOut', 'Elevation averaging ranges', data_type='string',
                          abstract='Elevation range where the particle number is counted (m agl)'
                                   " Example: 0-100",
@@ -60,16 +40,10 @@ class RunNAME(Process):
             LiteralInput('resolution','Resolution', data_type='float',
                          abstract='degrees, note the UM global Met data was only 17Km resolution',
                          allowed_values=[0.05,0.25], default=0.25, min_occurs=0),
-            LiteralInput('timestamp','timestamp of runs', data_type='string',
-                         abstract='how often the prog will run?',
-                         allowed_values=['3-hourly','daily']),
-            LiteralInput('dailytime','daily run time', data_type='time',
-                         abstract='if running daily, at what time will it run',
-                         min_occurs = 0),
             LiteralInput('startdate', 'Start date of runs', data_type='date',
-                         abstract='start date of runs'),
+                         abstract='start date of runs (YYYY-MM-DD)'),
             LiteralInput('enddate', 'End date of runs', data_type='date',
-                         abstract = 'end date of runs'),
+                         abstract = 'end date of runs (YYYY-MM-DD)')
             ]
         outputs = [
             ComplexOutput('NAMEinput', 'Input file for running NAME',
@@ -84,10 +58,10 @@ class RunNAME(Process):
                           ),
             ]
 
-        super(RunNAME, self).__init__(
+        super(RunNAMEstandard, self).__init__(
             self._handler,
-            identifier='runname',
-            title='Run NAME-on-JASMIN',
+            identifier='runnamestd',
+            title='Run standard NAME-on-JASMIN',
             abstract="Passes input arguments onto NAME",
             version='0.1',
             metadata=[
@@ -111,31 +85,33 @@ class RunNAME(Process):
                     'The value "{}" does not contain a "-" character to define a range, '
                     'e.g. 0-100'.format(elevationrange.data))
 
-        # Process the string of domains into a list of floats.
-        domains = []
-        if ',' not in request.inputs['domain'][0].data:
-            raise InvalidParameterValue("The domain coordinates must be split using a ','")
-        for val in request.inputs['domain'][0].data.split(','):
-            domains.append(float(val))
-        if len(domains) != 4:
-            raise InvalidParameterValue("There must be four coordinates entered, minX,maxX,minY,maxY")
-
         # Might want to change the elevation input to something similar to this as well so we don't have three separate params
 
         params = dict()
         for p in request.inputs:
             if p == 'elevationOut':
                 params[p] = ranges
-            elif p == 'domain':
-                params[p] = domains
             else:
                 params[p] = request.inputs[p][0].data
+
+        if params['title'] == "CAPEVERDE":
+            params['longitude'] = 16.863611
+            params['latitude'] = -24.867222
+            params['domain'] = [-120.0,80.0,-30.0,90.0]
+        elif params['title'] == "BEIJING":
+            params['longitude'] = 100.9
+            params['latitude'] = 36.28
+            params['domain'] = [30.0, 170.0, -10.0, 80.0]
+
+        params['elevation'] = 100
+        params['timeFmt'] = "days"
+        params['timestamp'] = '3-hourly'
+
 
         runtype = "FWD"
         if params['runBackwards']:
             runtype = "BCK"
 
-        lastfile = ""
         # Will loop through all the dates in range, including the final day
         for cur_date in daterange(request.inputs['startdate'][0].data,
                                   request.inputs['enddate'][0].data + timedelta(days=1)):
