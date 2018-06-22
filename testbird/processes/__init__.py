369a65b2d3affa89df64b5a17c9218e0f4fe3798
TeriForey <teri.forey@gmail.com>
2018-06-22 11:46:09 +0100

    Removed test and default processes, their tests and requirements from enviroment.yml

--- testbird/processes/__init__.py
+++ testbird/processes/__init__.py
@@ -1,6 +1,3 @@
-from .wps_wordcounter import WordCounter
-from .wps_inout import InOut
-from .wps_testplot import MakeMap
 from .wps_nameparams import RunNAME
 from .wps_name_standard import RunNAMEstandard
 from .wps_plot_allops import PlotAll
